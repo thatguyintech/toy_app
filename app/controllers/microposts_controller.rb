bce08a44a72594d581cac372863d85325914955a
Albert Hu <alberthu@berkeley.edu>
2016-06-06 17:33:33 -0700

    Finish Users and migrate db for Microposts

---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,92 @@
+class MicropostsController < ApplicationController
+  # GET /microposts
+  # GET /microposts.json
+  def index
+    @microposts = Micropost.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @microposts }
+    end
+  end
+
+  # GET /microposts/1
+  # GET /microposts/1.json
+  def show
+    @micropost = Micropost.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @micropost }
+    end
+  end
+
+  # GET /microposts/new
+  # GET /microposts/new.json
+  def new
+    @micropost = Micropost.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @micropost }
+    end
+  end
+
+  # GET /microposts/1/edit
+  def edit
+    @micropost = Micropost.find(params[:id])
+  end
+
+  # POST /microposts
+  # POST /microposts.json
+  def create
+    @micropost = Micropost.new(micropost_params)
+
+    respond_to do |format|
+      if @micropost.save
+        format.html { redirect_to @micropost, notice: 'Micropost was successfully created.' }
+        format.json { render json: @micropost, status: :created, location: @micropost }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @micropost.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /microposts/1
+  # PATCH/PUT /microposts/1.json
+  def update
+    @micropost = Micropost.find(params[:id])
+
+    respond_to do |format|
+      if @micropost.update_attributes(micropost_params)
+        format.html { redirect_to @micropost, notice: 'Micropost was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @micropost.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /microposts/1
+  # DELETE /microposts/1.json
+  def destroy
+    @micropost = Micropost.find(params[:id])
+    @micropost.destroy
+
+    respond_to do |format|
+      format.html { redirect_to microposts_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+    # Use this method to whitelist the permissible parameters. Example:
+    # params.require(:person).permit(:name, :age)
+    # Also, you can specialize this method with per-user checking of permissible attributes.
+    def micropost_params
+      params.require(:micropost).permit(:content, :user_id)
+    end
+end
